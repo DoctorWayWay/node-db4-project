79af95d18cde17fc9a8ff3f04bf736da664087a4
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-09 17:18:05 -0800

    added FK and additional columns to steps table

--- data/migrations/20211210002732_first-migration.js
+++ data/migrations/20211210002732_first-migration.js
@@ -13,10 +13,21 @@ exports.up = async function (knex) {
         .notNullable()
         .unique()
       table.string("ingredient_unit", 64)
-        
+
     })
     .createTable("steps", table => {
-      table.increments()
+      table.increments("step_id")
+      table.string("step_instructions")
+        .notNullable()
+      table.integer("step_number")
+        .notNullable()
+      table.integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipe_id")
+        .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT")
     })
     .createTable("step_ingredients", table => {
       table.increments()
