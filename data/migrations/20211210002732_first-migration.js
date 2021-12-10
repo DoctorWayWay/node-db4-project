b5c1d1f4550b28f9a9c6df3014650cd16861520b
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-09 17:12:34 -0800

    added more to first migration schema

--- data/migrations/20211210002732_first-migration.js
+++ data/migrations/20211210002732_first-migration.js
@@ -2,10 +2,18 @@
 exports.up = async function (knex) {
   return await knex.schema
     .createTable("recipes", table => {
-      table.increments()
+      table.increments("recipe_id")
+      table.string("recipe_name", 200)
+        .notNullable()
+        .unique()
     })
     .createTable("ingredients", table => {
-      table.increments()
+      table.increments("ingredient_id")
+      table.string("ingredient_name", 128)
+        .notNullable()
+        .unique()
+      table.string("ingredient_unit", 64)
+        
     })
     .createTable("steps", table => {
       table.increments()
