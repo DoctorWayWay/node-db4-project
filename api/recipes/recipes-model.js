2ecfbb8db16e75d32fb364992c9b7966728d4d25
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-12-12 13:12:01 -0800

    getRecipeById EP now returns steps

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -12,18 +12,44 @@ async function getRecipeById(recipe_id) {
     .where("r.recipe_id", recipe_id)
     .orderBy("s.step_number", "asc")
 
-  return recipeRows
-  /*
-    select r.recipe_id, r.recipe_name, r.created_at,
-    s.step_id, s.step_instructions, s.step_number,
-    i.ingredient_id, i.ingredient_name, i.ingredient_unit,
-    si.quantity
-    from steps as s join recipes as r on s.recipe_id = r.recipe_id
-    join step_ingredients as si on s.step_id = si.step_id
-    join ingredients as i on si.ingredient_id = i.ingredient_id
-    where r.recipe_id = 1
-    order by s.step_number asc;
-  */
+  const ingredients = recipeRows.map(step => {
+    return {
+      ingredient_name: step.ingredient_name,
+      ingredient_id: step.ingredient_id,
+      ingredient_unit: step.ingredient_unit,
+      step_number: step.step_number
+    }
+  })
+
+  let stepTotal = 0
+  const uniqueSteps = []
+
+  for (let step of recipeRows) {
+    if (stepTotal < step.step_number) {
+      stepTotal = step.step_number
+      uniqueSteps.push({
+        step_id: step.step_id,
+        step_number: step.step_number,
+        step_instructions: step.step_instructions,
+        ingredients: ingredients.filter(ing => {
+          return ing.step_number === step.step_number
+        })
+          .map(step => {
+            if (!step.ingredient_id) {
+              return []
+            } else {
+              return {
+                ingredient_name: step.ingredient_name,
+                ingredient_id: step.ingredient_id,
+                ingredient_unit: step.ingredient_unit
+              }
+            }
+          })
+      })
+    }
+  }
+
+  return uniqueSteps
 }
 
 module.exports = {
